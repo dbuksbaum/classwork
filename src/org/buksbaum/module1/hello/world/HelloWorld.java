05420698c80ae884c7b06d9397573547cf57e932
David Buksbaum <david.buksbaum@gmail.com>
2015-02-06 15:18:05 -0500

    Work for IT 620 - Module 1

---
+++ src/org/buksbaum/module1/hello/world/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.buksbaum.module1.hello.world;
+
+/**
+ * Hello world class implemented as part of module 1
+ */
+public class HelloWorld
+{
+  /**
+   * Entry point to the HelloWorld console app
+   * @param args Command line arguments to main
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world IT620 On-Line");
+    System.out.println("This is the first argument " + args[0]);
+  }
+}
