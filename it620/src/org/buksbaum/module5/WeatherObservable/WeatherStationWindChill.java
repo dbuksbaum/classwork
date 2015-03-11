9f5511b71fd2a51d67d5abbf13067d2cba572447
David Buksbaum <david.buksbaum@gmail.com>
2015-03-11 18:39:50 -0400

    Work for IT 620 - Added some comments

--- it620/src/org/buksbaum/module5/WeatherObservable/WeatherStationWindChill.java
+++ it620/src/org/buksbaum/module5/WeatherObservable/WeatherStationWindChill.java
@@ -2,6 +2,8 @@ package org.buksbaum.module5.WeatherObservable;
 
 /**
  * Created by david on 3/7/2015.
+ * Test program to display the Wind Chill. Copied from
+ * the WeatherStationHeatIndex.java class
  */
 public class WeatherStationWindChill {
 
@@ -11,8 +13,10 @@ public class WeatherStationWindChill {
     StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
     ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
     HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+    //  register the Wind Chill Display
     WindChillDisplay windChillDisplay = new WindChillDisplay(weatherData);
 
+    //  values copied from the assignment
     weatherData.setMeasurements(10, 40, 30.5f, 20);
     weatherData.setMeasurements(0, 30, 30.7f, 10);
     weatherData.setMeasurements(-10, 20, 29.8f, 15);
