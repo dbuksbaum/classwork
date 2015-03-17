84a2ee8cc7d9e7ac7e0c5cdc5ffddfcfb3023f1d
David Buksbaum <david.buksbaum@gmail.com>
2015-03-16 20:11:33 -0400

    Work for IT 620 -  Module 6 StarBuzz Caramel added

--- it620/src/org/buksbaum/module6/starbuzz/Caramel.java
+++ it620/src/org/buksbaum/module6/starbuzz/Caramel.java
@@ -3,19 +3,18 @@ package org.buksbaum.module6.starbuzz;
 /**
  * Created by david on 3/16/2015.
  */
-public class Soy extends CondimentDecorator {
+public class Caramel extends CondimentDecorator {
   Beverage beverage;
 
-  public Soy(Beverage beverage) {
+  public Caramel(Beverage beverage) {
     this.beverage = beverage;
   }
 
   public String getDescription() {
-    return beverage.getDescription() + ", Soy";
+    return beverage.getDescription() + ", Caramel";
   }
 
   public double cost() {
-    return .15 + beverage.cost();
+    return .20 + beverage.cost();
   }
 }
-
--- it620/src/org/buksbaum/module6/starbuzz/StarbuzzCoffee.java
+++ it620/src/org/buksbaum/module6/starbuzz/StarbuzzCoffee.java
@@ -23,5 +23,47 @@ public class StarbuzzCoffee {
     beverage3 = new Whip(beverage3);
     System.out.println(beverage3.getDescription()
                                + " $" + beverage3.cost());
+
+    //  DTB: added code here
+    //  Espresso + 2 Soy + 1 Whip
+    Beverage beverage4 = new Espresso();
+    //  our 2 soys
+    beverage4 = new Soy(beverage4);
+    beverage4 = new Soy(beverage4);
+    //  our 1 whip
+    beverage4 = new Whip(beverage4);
+    System.out.println(beverage4.getDescription() + " $" + beverage4.cost());
+
+    //  Dark Roast + 1 Mocha + 1 Soy + 2 Whip
+    Beverage beverage5 = new DarkRoast();
+    //  our 1 mocha
+    beverage5 = new Mocha(beverage5);
+    //  our 1 soy
+    beverage5 = new Soy(beverage5);
+    //  our 2 whips
+    beverage5 = new Whip(beverage5);
+    beverage5 = new Whip(beverage5);
+    System.out.println(beverage5.getDescription() + " $" + beverage5.cost());
+
+    //  House Blend + 2 Caramels + 1 Soy + 1 Whip
+    Beverage beverage6 = new HouseBlend();
+    //  our 2 caramels
+    beverage6 = new Caramel(beverage6);
+    beverage6 = new Caramel(beverage6);
+    //  our 1 soy
+    beverage6 = new Soy(beverage6);
+    //  our 1 whip
+    beverage6 = new Whip(beverage6);
+    System.out.println(beverage6.getDescription() + " $" + beverage6.cost());
+
+    //  Dark Roast + 1 Mocha + 1 Caramels + 1 Whip
+    Beverage beverage7 = new DarkRoast();
+    //  our 1 mocha
+    beverage7 = new Mocha(beverage7);
+    //  our 1 caramel
+    beverage7 = new Caramel(beverage7);
+    //  our 1 whip
+    beverage7 = new Whip(beverage7);
+    System.out.println(beverage7.getDescription() + " $" + beverage7.cost());
   }
 }
